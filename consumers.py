6de23ead882eb2d69c906f4984f7414b731a48da
Vitaliy <vitasiamen@gmail.com>
2023-04-07 23:07:37 +0300

    pre adventure

---
+++ consumers.py
@@ -0,0 +1,44 @@
+import json
+
+def create_delivery(state, event):
+    data = json.loads(event.data)
+    return {
+        "id": event.delivery_id,
+        "budget": int(data['budget']),
+        "notes": data['notes'],
+        "status": "ready"
+    }
+
+def start_delivery(state, event):
+    return state | {
+        "status": "active"
+    }
+
+def pickup_products(state, event):
+    data = json.loads(event.data)
+    new_budget = state['budget'] - int(data['purchase_price'])*int(data['quantity'])
+    return state | {
+        "budget": new_budget,
+        "purchase_price": int(data['purchase_price']),
+        "quantity": int(data['quantity']),
+        "status": "collected"
+    }
+
+
+def deliver_products(state, event):
+    data = json.loads(event.data)
+    new_budget = state['budget'] - int(data['sell_price']) * int(data['quantity'])
+    new_quantity = state['quantity'] - int(data['quantity'])
+    return state | {
+        "budget": new_budget,
+        "sell_price": int(data['sell_price']),
+        "quantity": new_quantity,
+        "status": "completed"
+    }
+
+CONSUMERS = {
+    "CREATE_DELIVERY": create_delivery,
+    "START_DELIVERY": start_delivery,
+    "PICKUP_PRODUCTS": pickup_products,
+    "DELIVER_PRODUCTS": deliver_products
+}
--- mainproject/urls.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from .controllers import (
-    create_delivery
-)
-router = APIRouter()
-
-router.add_api_route('/deliveries/create', create_delivery, methods=['POST'])
